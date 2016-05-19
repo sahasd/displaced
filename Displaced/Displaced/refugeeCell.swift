73275afb5c4b5c1e6388b62f1dc17957895e0d16
sahasd <sahasd@gmail.com>
2016-05-18 23:24:12 -0700

    Added initial game files

---
+++ Displaced/Displaced/refugeeCell.swift
@@ -0,0 +1,24 @@
+//
+//  refugeeCell.swift
+//  Displaced
+//
+//  Created by Sahas D on 5/18/16.
+//  Copyright © 2016 Sahas D. All rights reserved.
+//
+
+import UIKit
+
+class refugeeCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
